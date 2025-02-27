1e765d79d1ef8f66928b8de55445794d4dfa47df
CyberVi00 <159148762+VitaFrnkn@users.noreply.github.com>
2025-02-27 22:12:43 +0900

    adding formatted date and time from api call

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//API integration, function will call the data of the city searched and display on the interface
 function refreshWeather(response){
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
@@ -5,18 +6,32 @@ function refreshWeather(response){
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity-value");
     let windElement = document.querySelector("#wind-value");
+    let timeElement = document.querySelector("#time")
+    let date = new Date(response.data.time *1000); //date data needs to be formatted
 
-
-    cityElement.innerHTML = response.data.city;
-    descriptionElement.innerHTML = response.data.condition.description;
-    humidityElement.innerHTML = response.data.temperature.humidity;
-    console.log(response.data.temperature);
-    windElement.innerHTML = response.data.wind.speed;
-    
-    temperatureElement.innerHTML = Math.round(temperature);
+    cityElement.innerHTML = response.data.city;//get the city
+    timeElement.innerHTML = formatDate(date); // adding the date and time elements
+    descriptionElement.innerHTML = response.data.condition.description; // add description element
+    humidityElement.innerHTML = response.data.temperature.humidity; //add humidity details
+    windElement.innerHTML = response.data.wind.speed; //add wind details
+    temperatureElement.innerHTML = Math.round(temperature); //add rounded temperature
 }
 
-//display the temperature of the searched city, integrating API
+// formatting the date
+function formatDate(date){
+    
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+
+    if (minute<10){
+        minute = `0${minute}`;
+    }
+    
+    let days = ["Sunday","Monday",'Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    let day = days[date.getDay()];
+
+    return `${day} ${hour}:${minute}`;
+}
 
 function searchCity(city){
     //make api call and update interface
